009dd7b98b9e04b9c246893c245c9e06696a8c68
Milorad Vulovic <vulovic.ra166.2018@uns.ac.rs>
2022-03-10 12:50:07 +0100

    Update view of instructor main page

--- angular/src/app/profile-admin/profile-admin.component.ts
+++ angular/src/app/profile-admin/profile-admin.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core'
-import { HttpErrorResponse} from '@angular/common/http';
-import { UserService } from '../service/user.service';
-import { User } from '../model/user';
-import { ActivatedRoute, Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core'
+import {UserService} from '../service/user.service';
+import {User} from '../model/user';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-profile-admin',
@@ -13,56 +12,54 @@ export class ProfileAdminComponent implements OnInit {
   user: User = new User();
   id!: number;
   //@ts-ignore
-  currentUser = JSON.parse(localStorage.getItem('currentUser')); 
-  constructor(
-	private route: ActivatedRoute,
-	private router: Router,
-	private userService: UserService
-	)
-  {  
-	}
+  currentUser = JSON.parse(localStorage.getItem('currentUser'));
 
-  ngOnInit(): void{
-	if(this.currentUser === null){
-	alert('Niste se ulogovali');
-	this.logOut();
-	}
-	else{
-	this.getUser();
-	}
-	}
-	
-	public getUser(): void{
-		const username = this.currentUser.username1;
-		this.userService.findUser(username).subscribe(
-		 (response: User) => {
-			 this.user = response;
-		 }
-		);
-	}
-	
-	public updateUser(): void{
-		if(this.user.password1 === undefined){
-			alert('Unesite lozinku');
-		}
-		else{
-		this.userService.updateUser(this.user).subscribe(
-			response=>{
-				alert('Uspesno ste izmenili podatke');
-				this.router.navigate(['/homeAdmin'])
-			}
-		);
-		}
-	}
-	
-	goBack(){
-		this.router.navigate(['/homeAdmin']);
-	}
-	
-	logOut(){
-		localStorage.removeItem('currentUser');
-		localStorage.clear();
-		this.router.navigate(['/login']);
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private userService: UserService
+  ) {
+  }
+
+  ngOnInit(): void {
+    if (this.currentUser === null) {
+      alert('Niste se ulogovali');
+      this.logOut();
+    } else {
+      this.getUser();
+    }
+  }
+
+  public getUser(): void {
+    const username = this.currentUser.username1;
+    this.userService.findUser(username).subscribe(
+      (response: User) => {
+        this.user = response;
+      }
+    );
+  }
+
+  public updateUser(): void {
+    if (this.user.password1 === undefined) {
+      alert('Unesite lozinku');
+    } else {
+      this.userService.updateUser(this.user).subscribe(
+        response => {
+          alert('Uspesno ste izmenili podatke');
+          this.router.navigate(['/homeAdmin'])
+        }
+      );
+    }
+  }
+
+  goBack() {
+    this.router.navigate(['/homeAdmin']);
+  }
+
+  logOut() {
+    localStorage.removeItem('currentUser');
+    localStorage.clear();
+    this.router.navigate(['/login']);
   }
 
 }
